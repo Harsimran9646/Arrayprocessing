19ba2c51eb536f68276b6dd4be7fa2b107604606
Harsimran Singh <707995@student.tor.loc>
2019-04-18 14:17:02 -0400

    commit

---
+++ Arrayprocessing/Arrayprocessing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrayprocessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+        }
+
+        static void printRepeating(int[]arr, int size)
+            {
+            int i, j;
+        Console.WriteLine("Element which occur three times");
+            for (i = 0; i < size; i++)
+            {
+            for (j = i+1; j < size; j++)
+            {
+            if (arr[i] ==arr[j++])
+            Console.WriteLine(arr[i]+" ");
+            }
+            }
+       
+            }
+    }
+}
